6dfbc275f64d424d0495eab27d8d3b19625e62a4
徐彦龙 <yanlongxu@jieyuechina.com>
2018-07-31 14:33:57 +0800

    创建项目

---
+++ YLCalendar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YLCalendar
+//
+//  Created by McIntosh on 2018/7/31.
+//  Copyright © 2018年 xyanl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
